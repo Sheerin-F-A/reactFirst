9ddf3f6b9acca99415616ef8ac527e703909b592
Sheerin-F-A <69601466+Sheerin-F-A@users.noreply.github.com>
2025-03-13 16:23:35 +0530

    Create MOV f'm first version

---
+++ src/Greeting.jsx
@@ -0,0 +1,13 @@
+function Greeting() {
+    return (
+        <div>
+    <h1>Antonio said,</h1>
+    <em><p>&quot;In sooth, I know not why I am so sad.
+It wearies me; you say it wearies you.
+But how I caught it, found it, or came by it,
+What stuff ’tis made of, whereof it is born,
+I am to learn.&quot;</p></em>
+</div>
+);
+}
+export default Greeting;
